5d292f9294975ee61aca5c9fbf3f857f7978d03a
Thauhidmahmud <thauhidmahmud2000@gmail.com>
2026-04-18 14:43:40 +0600

    setup py file updated

--- setup.py
+++ setup.py
@@ -17,23 +17,36 @@ import setuptools
 
 
 
+import setuptools
+
+# README ফাইল থেকে লং ডেসক্রিপশন পড়া হচ্ছে
 with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
+# আপনার প্যাকেজের ভার্সন এবং অন্যান্য তথ্য
+__version__ = "0.0.1"
+REPO_NAME = "ColabMediaSuite"
+SRC_REPO = "ColabMediaSuite"
+
 setuptools.setup(
-    name="ColabMediaSuite",
-    version="0.0.1",
+    name=SRC_REPO,
+    version=__version__,
     author="Thauhid Mahmud",
-    author_email="your.email@example.com",
-    description="A small package for rendering media in Colab/Jupyter",
+    author_email="thauhidmahmud2000@gmail.com",
+    description="A small python package for rendering media in Colab/Jupyter",
     long_description=long_description,
-    long_description_content_type="text/markdown", # এই লাইনটি খুব গুরুত্বপূর্ণ
-    url="https://github.com/ThauhidMahmud/ColabMediaSuite",
-    packages=setuptools.find_packages(),
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/ThauhidMahmud/{REPO_NAME}",
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    install_requires=[
+        "ensure==1.0.2",
+        "py-youtube==1.1.7"
+    ],
+    python_requires='>=3.8',
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
 )
---
+++ src/ColabMediaSuite/__init__.py
@@ -0,0 +1,6 @@
+
+
+
+# src/ColabMediaSuite/__init__.py
+from .youtube import render_youtube_video
+from .site import render_site
